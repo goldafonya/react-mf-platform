284a240d6137422da82ccf11aac770e64e82f536
Aleksander <aafonin@Aleksanders-MacBook-Pro-2.local>
2021-07-26 16:51:56 +0300

    test build ie11

--- ui-platform/src/components/login/Login.tsx
+++ ui-platform/src/components/login/Login.tsx
@@ -1,30 +1,6 @@
 import React, {FC} from "react";
 import Container from "@material-ui/core/Container";
 import {useAuth} from "../../hooks/useAuth";
-import {makeAutoObservable} from "mobx";
-import {observer} from "mobx-react";
-
-class Timer {
-    secondsPassed = 0
-
-    constructor() {
-        makeAutoObservable(this)
-    }
-
-    increase() {
-        this.secondsPassed += 1
-    }
-
-    reset() {
-        this.secondsPassed = 0
-    }
-}
-
-const myTimer = new Timer();
-
-const TimerView = observer(({timer}: { timer: Timer }) => (
-    <button onClick={() => timer.reset()}>Seconds passed: {timer.secondsPassed}</button>
-));
 
 const Login: FC = () => {
     const {auth} = useAuth();
@@ -32,7 +8,6 @@ const Login: FC = () => {
     return (
         <Container maxWidth="md">
             Login
-            <TimerView timer={myTimer}/>
         </Container>
 
     );
